3837c47f078f4e7928b0615fbfc737cc38acbd12
Siddhi-2000 <121499582+Siddhi-2000@users.noreply.github.com>
2024-05-11 23:54:06 +0530

    Update urls.py

--- home/urls.py
+++ home/urls.py
@@ -19,7 +19,7 @@ urlpatterns = [
 
 
 
-    path('viewevent', views.category_list, name='viewevent'),
+   path('viewevent', views.category_list, name='category_list'),
     path('categories/create/', views.create_category, name='create_category'),
     path('categories/<int:category_id>/', views.category_events, name='category_events'),
     path('categories/delete/<int:category_id>/', views.delete_category, name='delete_category'),
